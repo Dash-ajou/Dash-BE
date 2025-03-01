5b6079c3af9543e9ecf56020710be572a2d8ca09
waffle <nate.crema@gmail.com>
2025-03-01 23:39:33 +0900

    DASH-286/feat: 예외처리 예시양식 추가 및 가변메세지 대응메서드 추가

--- src/main/java/io/saim/dash/global/exception/ServiceExceptionContent.java
+++ src/main/java/io/saim/dash/global/exception/ServiceExceptionContent.java
@@ -6,11 +6,17 @@ import lombok.Getter;
 public enum ServiceExceptionContent {
 
 	// 400
+	// DEFAULT_BAD_REQUEST(400, "잘못된 요청입니다. 확인 후 다시 시도해주세요."),
 
 	// 401
+	// UNAUTHORIZED(401, "인증되지 않았습니다"),
+	// AUTHORIZATION_EXPIRED(401, "인증이 만료되었습니다. 잠시 후 다시 시도해주세요"),
+
+	// 403
+	// BLOCKED_USER(403, "차단된 사용자입니다. 관리자에게 문의해주세요"),
 
 	// 404
-
+	// DATA_NOT_FOUND(404, "일치하는 데이터ID를 찾을 수 없습니다: %d"),
 
 
 	// 500
@@ -25,5 +31,10 @@ public enum ServiceExceptionContent {
 	ServiceExceptionContent(int statusCode, String message) {
 		this.statusCode = statusCode;
 		this.message = message;
+	}
+
+	public ServiceExceptionContent replaceArg(String[] ...args) {
+		this.message = String.format(this.message, args);
+		return this;
 	}
 }
