0f07fb41ea35e224d95f68824bceedd594de9aa7
waffle <nate.crema@gmail.com>
2025-03-23 18:00:56 +0900

    DASH-276/feat: repository 추가 (vendor, product)

--- src/main/java/io/saim/dash/coupon/repository/Product/ProductRepository.java
+++ src/main/java/io/saim/dash/coupon/repository/Product/ProductRepository.java
@@ -1,9 +1,14 @@
 package io.saim.dash.coupon.repository.Product;
 
+import java.util.List;
+import java.util.Optional;
+
 import io.saim.dash.coupon.model.Product;
 
 public interface ProductRepository {
-	Long save(Product product);
+	void save(Product product);
 
-	Product getById(Long id);
+	Optional<Product> getById(Long id);
+
+	List<Product> findAllById(List<Long> productIds);
 }
