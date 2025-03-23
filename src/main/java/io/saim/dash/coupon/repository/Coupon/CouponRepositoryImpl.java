0249f4a6245b8710701f0c46724f327e51ed3484
waffle <nate.crema@gmail.com>
2025-03-24 01:15:28 +0900

    DASH-344/feat: repository 추가 (Coupon)

--- src/main/java/io/saim/dash/coupon/repository/Coupon/CouponRepositoryImpl.java
+++ src/main/java/io/saim/dash/coupon/repository/Coupon/CouponRepositoryImpl.java
@@ -1,19 +1,32 @@
 package io.saim.dash.coupon.repository.Coupon;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import io.saim.dash.coupon.model.Coupon;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Repository
 public class CouponRepositoryImpl implements CouponRepository {
 
+	private final CouponJpaRepository couponJpaRepository;
+
 	@Override
-	public Long save(Coupon coupon) {
-		return 0L;
+	public void save(Coupon coupon) {
+		couponJpaRepository.save(coupon);
 	}
 
 	@Override
-	public Coupon getById(Long id) {
-		return null;
+	public Optional<Coupon> getById(Long id) {
+		return couponJpaRepository.findById(id);
+	}
+
+	@Override
+	public void saveAll(List<Coupon> issuedCoupons) {
+		couponJpaRepository.saveAll(issuedCoupons);
 	}
 }
+
