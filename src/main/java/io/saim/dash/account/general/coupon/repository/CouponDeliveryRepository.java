78e367bfcebddd3a584a649e27940d06c309e0c0
Soyoung Yang <ysbeam@naver.com>
2025-05-17 16:32:35 +0900

    feat: 보낸 쿠폰 목록 조회 API 구현

--- src/main/java/io/saim/dash/account/general/coupon/repository/CouponDeliveryRepository.java
+++ src/main/java/io/saim/dash/account/general/coupon/repository/CouponDeliveryRepository.java
@@ -6,5 +6,8 @@ import io.saim.dash.account.general.coupon.model.CouponDelivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponDeliveryRepository extends JpaRepository<CouponDelivery, Long> {
+	//보낸 쿠폰 목록 조회용
+	List<CouponDelivery> findBySenderId(Long senderId);
+	//받은 쿠폰 목록 조회용
 	List<CouponDelivery> findByReceiverIdAndStatus(Long receiverId, CouponDelivery.DeliveryStatus status);
 }
