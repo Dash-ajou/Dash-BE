2a5fe30f78179057177c029003304c87c1af2e2a
Soyoung Yang <ysbeam@naver.com>
2025-05-25 18:32:39 +0900

    refactor: 생성자 추가

--- src/main/java/io/saim/dash/account/partner/dto/CouponVendorDetailStatsDTO.java
+++ src/main/java/io/saim/dash/account/partner/dto/CouponVendorDetailStatsDTO.java
@@ -13,6 +13,13 @@ public class CouponVendorDetailStatsDTO {
 	private Long vendorIssued;
 	private Long vendorUsed;
 
+	public CouponVendorDetailStatsDTO(Long vendorId, String vendorName, Long vendorIssuedCount, Integer vendorUsedCount) {
+		this.vendorId = vendorId;
+		this.vendorName = vendorName;
+		this.vendorIssued = vendorIssuedCount;
+		this.vendorUsed = vendorUsedCount != null ? vendorUsedCount.longValue() : 0L;
+	}
+
 	public double getVendorUsageRate() {
 		if (vendorIssued == null || vendorIssued == 0L) return 0.0;
 		return Math.round((vendorUsed * 1000.0 / vendorIssued)) / 10.0; // 소수점 첫째 자리
