cfa9ee5224a19047d1b03a9fcaf19be20a4ab047
Soyoung Yang <ysbeam@naver.com>
2025-06-04 23:00:04 +0900

    refactor: 통계 리스트

--- src/main/java/io/saim/dash/account/partner/dto/MenuUsageStatsDTO.java
+++ src/main/java/io/saim/dash/account/partner/dto/MenuUsageStatsDTO.java
@@ -4,10 +4,12 @@ package io.saim.dash.account.partner.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class MenuUsageStatsDTO {
 	private String menuName;
 	private Long menuIssued;
