e97912d535cce9ab1c84b631b92a412550d1f6eb
Soyoung Yang <ysbeam@naver.com>
2025-06-04 12:57:08 +0900

    fix: 발급 단체 세부

--- src/main/java/io/saim/dash/coupon/common/repository/Vendor/VendorRepository.java
+++ src/main/java/io/saim/dash/coupon/common/repository/Vendor/VendorRepository.java
@@ -1,7 +1,9 @@
 package io.saim.dash.coupon.common.repository.Vendor;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import io.saim.dash.coupon.common.model.Vendor;
 
-public interface VendorRepository {
-	void save(Vendor vendor);
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+
 }
