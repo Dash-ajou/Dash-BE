5f0af85d0cde93d0442d8d0e78dea882c108a185
Soyoung Yang <ysbeam@naver.com>
2025-06-06 20:48:56 +0900

    refactor: 응답형식 수정

--- src/main/java/io/saim/dash/account/auth/dto/LoginResponseDTO.java
+++ src/main/java/io/saim/dash/account/auth/dto/LoginResponseDTO.java
@@ -1,22 +1,46 @@
 package io.saim.dash.account.auth.dto;
 
 import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponseDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private Long userId;
-	private String userName;
-	private String userEmail;
-	private String userPhone;
-	private String userType;
+	private User user;
+
+	@JsonProperty("session_id")
 	private String sessionId;
+
+	public Long getUserId() {
+		return this.user != null ? this.user.getUserId() : null;
+	}
+
+	@Getter @Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class User implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		@JsonProperty("user_id")
+		private Long userId;
+
+		@JsonProperty("user_name")
+		private String userName;
+
+		@JsonProperty("user_email")
+		private String userEmail;
+
+		@JsonProperty("user_phone")
+		private String userPhone;
+
+		@JsonProperty("user_type")
+		private String userType;
+	}
 }
