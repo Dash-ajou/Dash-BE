7275ebd778fadaf6002597f9e38e7aa6062293d5
Soyoung Yang <ysbeam@naver.com>
2025-06-06 18:41:51 +0900

    refactor: login 응답

--- src/main/java/io/saim/dash/account/auth/dto/LoginResponseDTO.java
+++ src/main/java/io/saim/dash/account/auth/dto/LoginResponseDTO.java
@@ -1,7 +1,5 @@
 package io.saim.dash.account.auth.dto;
 
-import java.io.Serializable;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,23 +7,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class LoginResponseDTO implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-	private User user;
+public class LoginResponseDTO {
+	private Long userId;
+	private String userName;
+	private String userEmail;
+	private String userPhone;
+	private String userType;
 	private String sessionId;
-
-	@Getter
-	@Setter
-	@AllArgsConstructor
-	public static class User implements Serializable {
-		private static final long serialVersionUID = 1L;
-
-		private Long userId;
-		private String userName;
-		private String userEmail;
-		private String userPhone;
-		private String userType;
-	}
 }
