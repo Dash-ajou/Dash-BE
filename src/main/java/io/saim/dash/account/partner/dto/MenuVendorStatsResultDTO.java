b9f97ecb8e1fe112f67f884eb3b99b0436fb76db
Soyoung Yang <ysbeam@naver.com>
2025-06-06 21:06:54 +0900

    refactor: 응답 형식 수정

--- src/main/java/io/saim/dash/account/partner/dto/MenuVendorStatsResultDTO.java
+++ src/main/java/io/saim/dash/account/partner/dto/MenuVendorStatsResultDTO.java
@@ -1,13 +1,20 @@
 package io.saim.dash.account.partner.dto;
 
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class MenuVendorStatsResultDTO {
+
+	@JsonProperty("menu_name")
 	private String menu_name;
+
+	@JsonProperty("menu_vendors")
 	private int menu_vendors;
 	private List<MenuVendorStatsResponseDTO> vendors;
 }
