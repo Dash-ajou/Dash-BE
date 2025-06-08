3bb99bbf379ecd34f06454eb5bec2dbc2b92baea
Soyoung Yang <ysbeam@naver.com>
2025-06-08 12:48:03 +0900

    refactor: 이메일 전송 시 문구 변경

--- src/main/java/io/saim/dash/global/util/EmailUtil.java
+++ src/main/java/io/saim/dash/global/util/EmailUtil.java
@@ -15,7 +15,7 @@ public class EmailUtil {
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(toEmail);
 		message.setSubject("[DASH] 이메일 인증 코드");
-		message.setText("인증 코드: " + code + "\n\n" + "해당 코드는 3분간 유효합니다.");
+		message.setText("인증 코드: " + code + "\n\n" + "해당 코드는 5분간 유효합니다.");
 		mailSender.send(message);
 	}
 }
