ff7625af3937dc5c0ff1c8ccfd5798f5aa0d399e
Soyoung Yang <ysbeam@naver.com>
2025-06-09 23:56:13 +0900

    fix: 사용 완료 필터링

---
+++ src/main/java/io/saim/dash/global/annotation/LoginPartnerUser.java
@@ -0,0 +1,9 @@
+package io.saim.dash.global.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LoginPartnerUser {
+}
